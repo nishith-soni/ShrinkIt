3760dceef16c83e584510fcba5b2b4619fe59468
nishith-soni <nishithsoni4444@gmail.com>
2026-01-07 16:31:51 -0500

    Initializing Repo

---
+++ Backend/src/main/java/com/project/shrinkit/ShrinkitApplication.java
@@ -0,0 +1,13 @@
+package com.project.shrinkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrinkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrinkitApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/shrinkit/ShrinkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shrinkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrinkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
