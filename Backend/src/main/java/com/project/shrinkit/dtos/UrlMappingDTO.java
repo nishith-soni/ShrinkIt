68d6ea0f5f9c8c15e70701f996d924e3346f916a
nishith-soni <nishithsoni4444@gmail.com>
2026-01-09 17:19:36 -0500

    Add URL shortening feature with user association

---
+++ Backend/src/main/java/com/project/shrinkit/dtos/UrlMappingDTO.java
@@ -0,0 +1,14 @@
+package com.project.shrinkit.dtos;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class UrlMappingDTO {
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount;
+    private LocalDateTime createdDate;
+    private String username;
+}
---
+++ Backend/src/main/java/com/project/shrinkit/repository/UrlMappingRepository.java
@@ -0,0 +1,14 @@
+package com.project.shrinkit.repository;
+
+import com.project.shrinkit.models.UrlMapping;
+import com.project.shrinkit.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User User);
+}
