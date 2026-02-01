eb9910a4fad765d2bbe00ea2517ce298a54872fb
nishith-soni <nishithsoni4444@gmail.com>
2026-01-31 20:21:15 -0500

    feat: add loading spinner and graph component to ShortenItem for enhanced user experience

--- Frontend/src/Dashboard/ShortenItem.jsx
+++ Frontend/src/Dashboard/ShortenItem.jsx
@@ -12,6 +12,8 @@ import { useStoreContext } from "../contextApi/ContextApi";
 import { useNavigate } from "react-router-dom";
 import api from "../api/api";
 import { useEffect } from "react";
+import { Hourglass } from "react-loader-spinner";
+import Graph from "./Graph";
 
 const subDomain = import.meta.env.VITE_REACT_SUBDOMAIN.replace(
   /^https?:\/\//,
@@ -138,7 +140,37 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
             analyticToggle ? "flex" : "hidden"
           }  max-h-96 sm:mt-0 mt-5 min-h-96 relative  border-t-1 w-[100%] overflow-hidden border-slate-300`}
         >
-          {loader ? <div></div> : <div></div>}
+          {loader ? (
+            <div className="min-h-[calc(450px-140px)] flex justify-center items-center w-full">
+              <div className="flex flex-col items-center gap-1">
+                <Hourglass
+                  visible={true}
+                  height="50"
+                  width="50"
+                  ariaLabel="hourglass-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  colors={["#306cce", "#72a1ed"]}
+                />
+                <p className="text-slate-700">Please Wait...</p>
+              </div>
+            </div>
+          ) : (
+            <>
+              {analyticsData.length === 0 && (
+                <div className="absolute flex flex-col  justify-center sm:items-center items-end  w-full left-0 top-0 bottom-0 right-0 m-auto">
+                  <h1 className=" text-slate-800 font-serif sm:text-2xl text-[15px] font-bold mb-1">
+                    No Data For This Time Period
+                  </h1>
+                  <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-[12px] text-slate-600 ">
+                    Share your short link to view where your engagements are
+                    coming from
+                  </h3>
+                </div>
+              )}
+              <Graph graphData={analyticsData} />
+            </>
+          )}
         </div>
       </React.Fragment>
     </div>
