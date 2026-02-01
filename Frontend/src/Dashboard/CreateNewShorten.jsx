89f158ccc283bc97a1c88f782d0dd95a1b518430
nishith-soni <nishithsoni4444@gmail.com>
2026-01-31 19:28:02 -0500

    feat: add functionality to fetch and display user's shortened URLs with analytics

--- Frontend/src/Dashboard/CreateNewShorten.jsx
+++ Frontend/src/Dashboard/CreateNewShorten.jsx
@@ -43,6 +43,7 @@ const CreateNewShorten = ({ setOpen, refetch }) => {
             });
           });
 
+          // await refetch();
           reset();
           setOpen(false);
     } catch (error) {
--- Frontend/src/hooks/useQuery.js
+++ Frontend/src/hooks/useQuery.js
@@ -1,6 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../api/api.js";
 
+export const useFetchMyShortUrls = (token, onError) => {
+  return useQuery({
+    queryKey: ["my-shortenurls"],
+    queryFn: async () => {
+      return await api.get(
+        "/api/urls/myurls",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
+        },
+      );
+    },
+    select: (data) => {
+      const sortedDate = data.data.sort(
+        (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+      );
+      return sortedDate;
+    },
+    onError,
+    staleTime: 5000,
+  });
+};
+
 export const useFetchTotalClicks = (token, onError) => {
   return useQuery({
     queryKey: ["url-totalclick"],
